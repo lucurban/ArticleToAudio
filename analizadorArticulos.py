e1dc613de42a5a28c66b52580cd26127c2c2651b
Lucas <lucurban@gmail.com>
2026-07-03 17:38:25 -0500

    Inicia el repositorio en Git de ArticleToAudio, un proyecto para construir un programa que convierte artículos de texto en audio.

---
+++ analizadorArticulos.py
@@ -0,0 +1,9 @@
+'''Este programa descarga y analiza los artículos de un sitio web, extrayendo 
+información relevante como el título, autor, fecha de publicación, cantidad de
+caracteres y resumen. Utiliza la biblioteca newspaper3K para procesar el HTML 
+y obtener los datos necesarios.'''
+
+#---Importar pauetes ---
+from newspaper import Article
+
+print("¡newspaper3k está instalada correctamente!")
---
+++ test.py
@@ -0,0 +1,3 @@
+from newspaper import Article
+
+print("¡newspaper3k está instalada correctamente!")
